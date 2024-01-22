78b6f43b4ca2642090f607efb911acfc9c36114a
shockwave49 <155154602+shockwave49@users.noreply.github.com>
2024-01-22 12:35:20 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,20 @@
+
+package employe.managment.system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/employemanagmentsystem","root","Ayushking495");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
